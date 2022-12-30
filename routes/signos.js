76c04bf54bb0e97af486e0e2d4237a371440a681
Daniel Alonso <dan555lvl@gmail.com>
2022-12-29 21:08:29 -0300

    teste no Render

--- routes/signos.js
+++ routes/signos.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/:signo', (req, res) =>{
-    res.sendFile(__dirname + `/signos/signo-de-${req.params.signo}.html`)
+    res.status(200).sendFile(__dirname + `/signos/signo-de-${req.params.signo}.html`)
+    res.status(404).send("<h1>A pagina não existe.</h1>")
 })
 
 module.exports = router;
